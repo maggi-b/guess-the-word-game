d590b55c929207ddd014954c3aa2ffedcc0766f8
maggi-bratton <maggi.meins@gmail.com>
2023-03-18 13:24:23 -0600

    Display letters and check if win

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ const message = document.querySelector(".message");
 //hidden button that will appear prompting the player to play again
 const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
+//array of all the letters the player guesses
 const guessedLetters = [];
 
 //shows placeholders for each letter that has not been guessed
@@ -65,6 +66,39 @@ const makeGuess = function(guess) {
     } else {
         guessedLetters.push(guess);   
         console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
     }
+};
 
+const showGuessedLetters = function () {
+    //Clear the list first
+    guessedLettersList.innerHTML = "";
+    for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersList.append(li);
+    }
+};
+
+//Replace the placeholders with the correct letters guessed
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+    } else {
+        revealWord.push("●");
+    }
+    wordInProgress.innerText = revealWord.join("");
+    checkIfWin();
+}};
+
+const checkIfWin = function () {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+    }
 };
